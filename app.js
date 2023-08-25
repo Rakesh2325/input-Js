fa7eb40484567bc9d4aff143bdff7c6afd6e9299
Rakesh2325 <rakeshroshankumar4@gmail.com>
2023-08-25 16:21:09 +0500

    add file

---
+++ app.js
@@ -0,0 +1,12 @@
+function addData(){
+    console.log(document.getElementById("Name").value);
+    console.log(document.getElementById("Father_Name").value);
+    console.log(document.getElementById("Email_Address").value);
+    console.log(document.getElementById("Password").value);
+    console.log(document.getElementById("CNIC_Number").value);
+    console.log(document.getElementById("Mobile_Number").value);
+    console.log(document.getElementById("Select_Courses").value);
+    console.log(document.getElementById("Select_Qualification").value);
+    console.log(document.getElementById("Select_Gender").value);
+    console.log(document.getElementById("Message").value);
+}
